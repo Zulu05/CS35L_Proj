5031abe8098ca8be7f386d3f0022b334d6fd7aba
geeick <109990888+geeick@users.noreply.github.com>
2025-10-29 13:19:52 -0700

    Changed files (App and components homePage/quizPage) for typescript :wq
    :wq

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {}
+      <div> <p> Testing if this works!!!</p></div>
+    </>
+  )
+}
+
+export default App

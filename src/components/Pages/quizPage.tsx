4f2155e37b79539468ae63c1a65a68a3240ee205
Pedro Langone <pedrolangone@yahoo.com.br>
2025-12-02 11:57:35 -0800

    Improved CSS file with AI

--- src/components/Pages/quizPage.tsx
+++ src/components/Pages/quizPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./quizPage.css";
+import "./quizPageNew.css";
 import { fetchTraits } from "../../services/traits.service";
 import { TraitDefinition } from "../../models/traits";
 

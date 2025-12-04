9c3fe12ee886bd8b12b241962971fef289bb9af8
Hannah Park <hpark06@ucla.edu>
2025-12-04 12:30:13 -0800

    added step definition template for f8

---
+++ src/step_definitions/stepDefinitionsf8.ts
@@ -0,0 +1,190 @@
+ // src/step_definitions/f07.steps.ts
+
+import { Given, When, Then } from "@cucumber/cucumber";
+import { expect } from "@playwright/test";
+const BASE_URL = "http://localhost:5173";
+
+
+// 3) Scenario: Test completed successfully to see results # src/features/f08.feature:7
+  //  ? Given the user has logged in successfully
+       
+         Given('the user has logged in successfully', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  //  ? When they finish the quiz
+       
+         When('they finish the quiz', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  //  ? And they press the Submit button
+       
+         When('they press the Submit button', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  //  ? Then they should be taken to the results page
+       
+         Then('they should be taken to the results page', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  //  ? And they should see their club matches
+       
+         Then('they should see their club matches', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+// 4) Scenario: After taking test, results should be saved to the profile from home page # src/features/f08.feature:14
+  //  ? Given the user has logged in successfully
+       
+        //  Given('the user has logged in successfully', function () {
+        //    // Write code here that turns the phrase above into concrete actions
+        //    return 'pending';
+        //  });
+       
+  //  ? And they are on the home page
+       
+         Given('they are on the home page', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  //  ? When they press the Profile Button
+       
+         When('they press the Profile Button', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  //  ? Then they should see their top club matches
+       
+         Then('they should see their top club matches', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  //  ? And they should match with the quiz
+       
+         Then('they should match with the quiz', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+// 5) Scenario: After taking test, results should be saved to the profile from banner # src/features/f08.feature:21
+  //  ? Given the user has logged in successfully
+       
+        //  Given('the user has logged in successfully', function () {
+        //    // Write code here that turns the phrase above into concrete actions
+        //    return 'pending';
+        //  });
+       
+  //  ? When they press the their username button
+       
+         When('they press the their username button', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  //  ? Then they should see their top club matches
+       
+        //  Then('they should see their top club matches', function () {
+        //    // Write code here that turns the phrase above into concrete actions
+        //    return 'pending';
+        //  });
+       
+  //  ? And they should match with the quiz
+       
+        //  Then('they should match with the quiz', function () {
+        //    // Write code here that turns the phrase above into concrete actions
+        //    return 'pending';
+        //  });
+       
+// 6) Scenario: User is able to take quiz and log out of their account # src/features/f08.feature:27
+  //  ? Given the user has logged in successfully
+       
+        //  Given('the user has logged in successfully', function () {
+        //    // Write code here that turns the phrase above into concrete actions
+        //    return 'pending';
+        //  });
+       
+  //  ? When they finish the quiz
+       
+        //  When('they finish the quiz', function () {
+        //    // Write code here that turns the phrase above into concrete actions
+        //    return 'pending';
+        //  });
+       
+  //  ? And they press the Submit button
+       
+        //  When('they press the Submit button', function () {
+        //    // Write code here that turns the phrase above into concrete actions
+        //    return 'pending';
+        //  });
+       
+  //  ? And they should be able to press the Logout button
+       
+         When('they should be able to press the Logout button', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  //  ? Then they should get redirected to the Home Page
+       
+         Then('they should get redirected to the Home Page', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+
+// 7) Scenario: User is able to log out of their account # src/features/f08.feature:34
+  //  ? Given the user has logged in successfully
+       
+        //  Given('the user has logged in successfully', function () {
+        //    // Write code here that turns the phrase above into concrete actions
+        //    return 'pending';
+        //  });
+       
+  //  ? When they should be able to press the Logout button
+       
+        //  When('they should be able to press the Logout button', function () {
+        //    // Write code here that turns the phrase above into concrete actions
+        //    return 'pending';
+        //  });
+       
+  //  ? Then they should get redirected to the Home Page
+       
+        //  Then('they should get redirected to the Home Page', function () {
+        //    // Write code here that turns the phrase above into concrete actions
+        //    return 'pending';
+        //  });
+       
+  //  ? And no longer access their profile
+       
+         Then('no longer access their profile', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+// 8) Scenario: Results not available after logging out from Club Info # src/features/f09.feature:7
+  //  ✔ Given the user is not logged in # src/step_definitions/stepDefinitionsf5.ts:12
+  //  ? When they press the Club Information Button
+       
+         When('they press the Club Information Button', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+  //  ? Then they should not be able to see any matching results
+       
+         Then('they should not be able to see any matching results', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
